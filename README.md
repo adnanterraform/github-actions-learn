64c6d9cdc2fe21f60bd360778babb823b7d22087
Nausheen Mahmood <noshi@Nausheens-MBP.localdomain>
2021-09-30 00:17:31 +0100

    remove folder

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # github-actions-learn
+
+<!-- Task
+
+Setup pull requests
+Terraform structure  -->
--- terraform/200Compute/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "web" {
-  ami           = var.ami_id
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
