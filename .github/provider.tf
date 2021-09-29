e29d74869593de27714f7cb4c27d56d60bc49ceb
Nausheen Mahmood <noshi@Nausheens-MBP.localdomain>
2021-09-29 14:08:33 +0100

    new work flow terraform

---
+++ .github/provider.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "eu-west-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "adnan-terraform-project-github-actions--state"
+    key    = "terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+
---
+++ .github/workflows/state.tf
@@ -0,0 +1,25 @@
+resource "aws_s3_bucket" "s3Bucket" {
+  bucket = "adnan-terraform-project-github-actions-state"
+  acl    = "public-read"
+
+  policy = <<EOF
+{
+  "Id": "MakePublic",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::adnan-terraform-project-github-actions-state/*",
+      "Principal": "*"
+    }
+  ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+  }
+}
