fa79edfc7fee393aba1577db5cddbee8e73e85d8
Nausheen Mahmood <noshi@Nausheens-MBP.localdomain>
2021-09-29 15:50:42 +0100

    remove s3 bucket

--- terraform/state.tf
+++ terraform/state.tf
@@ -1,26 +1,26 @@
 
-resource "aws_s3_bucket" "s3Bucket" {
-  bucket = "adnan-github-action-resources"
-  acl    = "public-read"
+# resource "aws_s3_bucket" "s3Bucket" {
+#   bucket = "adnan-github-action-resources"
+#   acl    = "public-read"
 
-  policy = <<EOF
-{
-  "Id": "MakePublic",
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:GetObject"
-      ],
-      "Effect": "Allow",
-      "Resource": "arn:aws:s3:::adnan-github-action-resources/*",
-      "Principal": "*"
-    }
-  ]
-}
-EOF
+#   policy = <<EOF
+# {
+#   "Id": "MakePublic",
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Action": [
+#         "s3:GetObject"
+#       ],
+#       "Effect": "Allow",
+#       "Resource": "arn:aws:s3:::adnan-github-action-resources/*",
+#       "Principal": "*"
+#     }
+#   ]
+# }
+# EOF
 
-  website {
-    index_document = "index.html"
-  }
-}
+#   website {
+#     index_document = "index.html"
+#   }
+# }
