734c61400afe2fb3eac0c6f15506d37e15aac94a
Adnan Mahmood <adna9623@C02Q60LJFVH6.local>
2021-10-05 20:17:09 +0100

    new workflow

---
+++ bin/plan.sh
@@ -0,0 +1,16 @@
+# !/bin/sh
+set -xe
+ENV="${1}"
+echo "Changes prepared for the ${ENV} Envirnonment."
+chmod +x bin/plan.sh
+cd terraform
+GIT_COMMIT_SHA=$(git rev-parse --verify HEAD)
+terraform init -input=false -var-file=${ENV}.tfvars -backend-config="prefix=mgcp-1288677-4c-ng-${ENV}"
+if [ "$GITHUB_BRANCH" = "master" ];
+then
+terraform plan -input=false -var-file=${ENV}.tfvars -out="terraform-$GIT_COMMIT_SHA.plan" -detailed-exitcode
+else
+terraform plan -input=false -var-file=${ENV}.tfvars -out="terraform-$GIT_COMMIT_SHA.plan"
+fi
+# for debugging, show the plan file exists
+ls -la "terraform-$GIT_COMMIT_SHA.plan"
