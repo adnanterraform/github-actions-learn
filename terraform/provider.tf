ee8ca88609fe7cabffbc89e430b08ccb1d1d73f5
Adnan Mahmood <adnan@Adnans-MBP.localdomain>
2022-02-10 23:15:51 +0000

    remove

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,16 +1,16 @@
-# provider "aws" {
-#   # version = "~> 2.0"
-#   region = "eu-west-1"
-# }
-#
-#
-# terraform {
-#   backend "s3" {
-#     bucket = "adnan-terraform-project-github-actions-state"
-#     key    = "terraform.tfstate"
-#     region = "eu-west-1"
-#   }
-# }
+provider "aws" {
+  # version = "~> 2.0"
+  region = "eu-west-1"
+}
+
+
+terraform {
+  backend "s3" {
+    bucket = "adnan-terraform-project-github-actions-state"
+    key    = "terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
 
 
 # tags =
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,3 +1,3 @@
-# ami_id = "ami-0d1bf5b68307103c2"
+ami_id = "ami-0d1bf5b68307103c2"
 
-# region = ""
+region = ""
